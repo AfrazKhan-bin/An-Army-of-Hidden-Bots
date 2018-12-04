1b70a9f8d1f8a8a862c2ae281fea1ca527a37072
HassanTariq <30321761+HassanTariq67@users.noreply.github.com>
2018-12-04 11:46:20 +0500

    Updated Code
    New code for new file detetction without GUI and a fixed scanning path.

---
+++ Iteration1/DetectNewDownload/DownloadApplication.py
@@ -0,0 +1,48 @@
+import os, time
+import Tkinter as tk
+import tkMessageBox
+
+path_to_watch = "C://Users//hassa/Downloads"
+
+root = tk.Tk()
+root.withdraw()
+
+before = []  	#Initial representation of directory
+after = []		#
+for dirpath,dirnames,folder in os.walk(path_to_watch):
+	for eachFile in folder:
+		before.append(eachFile)
+
+while 1:
+	added = ""
+	removed = ""
+	time.sleep(5)
+	for dirpath,dirnames,folder in os.walk(path_to_watch):
+		for eachFile in folder:
+			after.append(eachFile)
+
+	# print(after)
+
+	for f in after:
+		if not f in before:
+			# print (f)
+			added =  added + f + ","
+
+	for f in before:
+		if not f in after:
+			# print(f)
+			removed = "" + f + ","
+
+	if added:
+		print ("Added: " + ", ".join (added))
+		tkMessageBox.showinfo("Information","New File:" + added + " is added")
+
+	if removed: 
+		print ("Removed: " + ", ".join (removed))
+		tkMessageBox.showinfo("Information","File:" + removed + " is deleted")
+
+	before = after
+	after = []
+
+
+
